91b4dd8fcd38f373f7f30ca672ee1b01fb3ba86f
Antonella <antonella.beti@gmail.com>
2024-09-27 16:29:18 -0300

    se completo la conexion del login y register

--- client/src/register.js
+++ client/src/register.js
@@ -1,3 +1,28 @@
-// ! REALIZAR LA LÓGICA DE REGISTRO DE USUARIOS AQUÍ
-
 import "./style.css";
+
+//*Formulario de registro
+const $form = document.getElementById("register-form");
+
+
+$form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+
+    const formData = new FormData($form);
+    const entries = Object.fromEntries(formData.entries());
+    fetch("http://localhost:4321/api/sign-up", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            credencials: "include",
+        },
+        body: JSON.stringify(entries),
+    }).then((response) => {
+        if (response.ok) {
+            window.location.href = "/pages/login";
+        } else {
+            console.error(response);
+        }
+    })
+})
